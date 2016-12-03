3db416a4c06a9fd11db079b315456004627e26f0
Adam Erb <adam.erb@carmigo.com>
2016-12-03 16:38:15 -0500

    Rename to satisfy ocd.

--- src/q1/Part1.kts
+++ src/q1/Part1.kts
@@ -1,29 +1,17 @@
 package q1
 
 import java.io.File
-import java.util.*
 
-data class Point(var x: Int = 0, var y: Int = 0)
-
-var o = 0; val curr = Point()
-val previous = HashSet<Point>()
-File("in.txt").readText().split(", ").first {
-    o = ((o + if(it[0] == 'L') -90 else +90) % 360 + 360) % 360
+var o = 0; var x = 0; var y = 0;
+File("in.txt").readText().split(", ").forEach {
+    o += if(it[0] == 'L') -90 else +90
     val dist = it.drop(1).toInt()
-    repeat(dist) {
-        when(o) {
-            0 -> curr.y += 1
-            90 -> curr.x += 1
-            180 -> curr.y -= 1
-            270 -> curr.x -= 1
-        }
-        if(curr in previous) return@first true
-        else {
-            previous += curr.copy()
-        }
+    when((o % 360 + 360) % 360) {
+        0 -> y += dist
+        90 -> x += dist
+        180 -> y -= dist
+        270 -> x -= dist
     }
-    false
 }
 
-println("$curr dist:${Math.abs(curr.x) + Math.abs(curr.y)}")
-
+println("x:$x y:$y dist:${Math.abs(x) + Math.abs(y)}")
